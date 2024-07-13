650d03eb042a6e24641220cca9ef711a90750b60
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-07-13 12:33:13 +0200

    task2

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,17 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from '..';
+
+export const PhotosGallery = ({ images, openModal }) => {
+  return (
+    <Grid>
+      {images.map(({ id, avg_color, alt, src }) => (
+        <PhotosGalleryItem
+          key={id}
+          alt={alt}
+          src={src}
+          color={avg_color}
+          openModal={openModal}
+        />
+      ))}
+    </Grid>
+  );
 };
--- src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.jsx
@@ -1,3 +1,18 @@
-export const PhotosGalleryItem = () => {
-  return <h3>PhotosGalleryItem</h3>;
+import { GridItem } from '..';
+import s from './PhotosGalleryItem.module.css';
+export const PhotosGalleryItem = ({ src, alt, color, openModal }) => {
+  return (
+    <GridItem>
+      <div
+        className={s.thumb}
+        style={{ backgroundColor: color, borderColor: color }}
+      >
+        <img
+          src={src.large}
+          alt={alt}
+          onClick={() => openModal(src.large, alt)}
+        />
+      </div>
+    </GridItem>
+  );
 };
