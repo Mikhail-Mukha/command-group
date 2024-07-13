3f8b4177797aa67ddf3f85e065ae4c9190d48598
Mikhail-Mukha <misha500krut@gmail.com>
2024-07-13 12:08:27 +0300

    second_commit

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,41 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, Text } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    const fetchImages = async () => {
+      setLoading(true);
+      try {
+        const { photos, per_page, total_results } = await getPhotos(
+          query,
+          page,
+        );
+        console.log(photos);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchImages();
+  }, [page, query]);
+
+  const onHandleSubmit = value => {
+    setQuery(value);
+  };
+
   return (
     <>
+      <Form onSubmit={onHandleSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
